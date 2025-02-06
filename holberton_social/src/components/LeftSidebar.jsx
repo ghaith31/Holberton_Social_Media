871102b7694e91569a8ff073bf5345e0f79eb8eb
ghaith31 <gharsallighaith31@gmail.com>
2025-02-06 21:54:06 +0100

    Add authentication  and loader

--- holberton_social/src/components/LeftSidebar.jsx
+++ holberton_social/src/components/LeftSidebar.jsx
@@ -1,16 +1,22 @@
 import Link from 'next/link';
 import { HiHome } from 'react-icons/hi';
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+} from '@clerk/nextjs';
 
 export default function Sidebar() {
   return (
     <div className='flex flex-col p-1 justify-between h-screen items-center'>
       <div className='flex flex-col gap-4 p-1'>
-        <br></br>
+        <br />
         <Link href='/'>
           <img
-            src='https://user-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_630,w_1200,f_auto,q_auto/79001/331125_630361.png' // Image stockée dans /public/
+            src='https://user-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_630,w_1200,f_auto,q_auto/79001/331125_630361.png'
             alt='Holberton School Logo'
-            className= 'w-20 h-20 cursor-pointer hover:bg-gray-100 rounded-full transition-all duration-200'
+            className='w-20 h-20 cursor-pointer hover:bg-gray-100 rounded-full transition-all duration-200'
           />
         </Link>
         <Link
@@ -20,9 +26,23 @@ export default function Sidebar() {
           <HiHome className='w-7 h-7' />
           <span className='font-bold hidden xl:inline'>Home</span>
         </Link>
-        <button className='bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold'>
-          Sign in
-        </button>
+        {/* Utilisation des composants Clerk sans <button> externe */}
+        <div className='w-48 h-9'>
+          <SignedIn>
+            <SignOutButton
+              className='bg-red-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold flex items-center justify-center'
+            >
+              Sign Out
+            </SignOutButton>
+          </SignedIn>
+          <SignedOut>
+            <SignInButton
+              className='bg-red-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold flex items-center justify-center'
+            >
+              Sign In
+            </SignInButton>
+          </SignedOut>
+        </div>
       </div>
     </div>
   );
