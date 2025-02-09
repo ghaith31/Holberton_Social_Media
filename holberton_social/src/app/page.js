1e8852ab7e13369862317a8918801b57c72299b2
ghaith31 <gharsallighaith31@gmail.com>
2025-02-09 04:54:26 +0100

    test

--- holberton_social/src/app/page.js
+++ holberton_social/src/app/page.js
@@ -17,7 +17,7 @@ export default async function Home() {
   return (
     <div className='min-h-screen max-w-xl mx-auto border-r border-l'>
       <div className='py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200'>
-        <h2 className='text-lg sm:text-xl font-bold'>Home</h2>
+        <h2 className='text-lg sm:text-xl font-bold'>  Holberton Social Media </h2>
       </div>
       <Input />
       <Feed data={data} />
