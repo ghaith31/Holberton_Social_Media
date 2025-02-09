ad17645eb34d7c2a3c0b76ea71d5dc17cfe7dce2
ghaith31 <gharsallighaith31@gmail.com>
2025-02-09 05:55:26 +0100

    fix titre

--- holberton_social/src/app/page.js
+++ holberton_social/src/app/page.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Input from '@/components/Input';
 import Feed from '@/components/Feed';
-
+import Image from 'next/image';
 
 export default async function Home() {
   let data = null;
@@ -16,8 +16,16 @@ export default async function Home() {
   }
   return (
     <div className='min-h-screen'>
-      <div className='py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200'>
-        <h2 className='text-lg sm:text-xl font-bold'>  Holberton Social Media </h2>
+      <div className='py-4 px-6 sticky top-0 z-50 bg-white/95 backdrop-blur-md border-b border-gray-200 shadow-sm'>
+        <div className='flex items-center space-x-3'>
+          
+          <div className='relative h-9 w-9 hover:scale-105 transition-transform'>
+            
+          </div>
+          <h2 className='text-2xl font-extrabold bg-gradient-to-r from-red-600 to-red-200 bg-clip-text text-transparent tracking-tight  text-cente'>
+          Holberton Social Media
+          </h2>
+        </div>
       </div>
       <Input />
       <Feed data={data} />
