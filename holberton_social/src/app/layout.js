8e32c76d76023df4d778ce275cad4d9ceb159118
ghaith31 <gharsallighaith31@gmail.com>
2025-05-03 22:14:38 +0200

    fix state management library 3

--- holberton_social/src/app/layout.js
+++ holberton_social/src/app/layout.js
@@ -24,26 +24,30 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-      <html lang='en'>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          <ClerkLoading>
-            <Loader />
-          </ClerkLoading>
-          <ClerkLoaded>
-          <div className='flex justify-between  mx-auto'>
-              <div className='hidden sm:inline border-r h-screen sticky top-0'>
-                <LeftSidebar />
+      <SessionWrapper>
+        <html lang='en'>
+          <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >
+            <ClerkLoading>
+              <Loader />
+            </ClerkLoading>
+            <ClerkLoaded>
+              <div className='flex justify-between max-w-6xl mx-auto'>
+                <div className='hidden sm:inline border-r h-screen sticky top-0'>
+                  <LeftSidebar />
+                </div>
+
+                <div className='w-2xl flex-1'>{children}</div>
+                <div className='lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]'>
+                  <RightSidebar />
+                </div>
               </div>
-              <div className='w-2xl flex-1'>{children}</div>
-              <div className='lg:flex-col p-1 h-screen border-l hidden lg:flex w-[34rem]'>
-                <RightSidebar />
-              </div>
-            </div>
-          </ClerkLoaded>
-        </body>
-      </html>
+              <CommentModal />
+            </ClerkLoaded>
+          </body>
+        </html>
+      </SessionWrapper>
     </ClerkProvider>
   );
 }
