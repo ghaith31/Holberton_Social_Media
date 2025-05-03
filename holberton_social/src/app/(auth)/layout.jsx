6c6397e29d1ce6bf2e93612080f6a8ad2a4bfaa0
ghaith31 <gharsallighaith31@gmail.com>
2025-05-03 22:08:35 +0200

    fix state management library 2

--- holberton_social/src/app/(auth)/layout.jsx
+++ holberton_social/src/app/(auth)/layout.jsx
@@ -1,11 +1,11 @@
-import '.././globals.css';
+// app/layout.js
+import '../globals.css';
+import { ClerkProvider, ClerkLoaded, ClerkLoading } from '@clerk/nextjs';
+import { RecoilRoot } from 'recoil';
 import Loader from '@/components/Loader';
-import { ClerkLoaded, ClerkLoading, ClerkProvider } from '@clerk/nextjs';
-
-import SessionWrapper from '@/components/SessionWrapper';
-import CommentModal from '@/components/CommentModal';
 import LeftSidebar from '@/components/LeftSidebar';
 import RightSidebar from '@/components/RightSidebar';
+import CommentModal from '@/components/CommentModal';
 
 export const metadata = {
   title: 'Next.js',
@@ -14,27 +14,25 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
-      <body className='antialiased'>
+    <html lang="en">
+      <body className="antialiased">
         <ClerkProvider>
           <ClerkLoading>
             <Loader />
           </ClerkLoading>
           <ClerkLoaded>
-            <SessionWrapper>
-              <div className='flex justify-between max-w-6xl mx-auto'>
-                <div className='hidden sm:inline border-r h-screen sticky top-0'>
+            <RecoilRoot>
+              <div className="flex justify-between max-w-6xl mx-auto">
+                <div className="hidden sm:inline border-r h-screen sticky top-0">
                   <LeftSidebar />
                 </div>
-
-                <div className='w-2xl flex-1'>{children}</div>
-
-                <div className='lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]'>
+                <div className="w-2xl flex-1">{children}</div>
+                <div className="lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]">
                   <RightSidebar />
                 </div>
               </div>
               <CommentModal />
-            </SessionWrapper>
+            </RecoilRoot>
           </ClerkLoaded>
         </ClerkProvider>
       </body>
